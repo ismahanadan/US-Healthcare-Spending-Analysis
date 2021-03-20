ce8dcc22b567b4d299c8ff2392b50ea2ec1e6db0
Laurel Williamson <laurelwilliamson@Laurels-MacBook-Pro.local>
2021-03-20 12:39:31 -0400

    Age.js aka ambulatory age chart

---
+++ Age.js
@@ -0,0 +1,27 @@
+// Initializes the page with a default plot
+//ambulatory
+function init() {
+    
+  
+    var trace1 = {
+      x: ['Under 15', '15-29', '30-39', '40-54', '55-69', '70-79', '80+'],
+      y: [179.17, 322.60, 405.30, 622.76, 1606.39, 1963.13, 1195.65],
+      name: 'Public',
+      type: 'bar'
+    };
+    
+    var trace2 = {
+      x: ['Under 15', '15-29', '30-39', '40-54', '55-69', '70-79', '80+'],
+      y: [295.86, 786.09, 1481.11, 1860.58, 2218.09, 576.41, 271.41],
+      name: 'Private',
+      type: 'bar'
+    };
+    
+    var data = [trace1, trace2];
+    
+    var layout = {barmode: 'group'};
+    
+    Plotly.newPlot('Public vs. Private Expenditure by Age Group', data, layout);
+  
+  init();
+  
